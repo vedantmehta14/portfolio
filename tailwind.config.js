9710c6cf5439a30e927554f5140783abe64cfa0a
Vedant Mehta <mehtavedant8@gmail.com>
2025-07-19 01:45:23 -0700

    changed px to rem

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,21 @@ module.exports = {
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
       },
+      fontSize: {
+        'xs': ['0.8125rem', { lineHeight: '1.25rem' }],    // ~13px (was 12px)
+        'sm': ['0.9375rem', { lineHeight: '1.375rem' }],   // ~15px (was 14px)
+        'base': ['1.075rem', { lineHeight: '1.625rem' }],  // ~17px (was 16px)
+        'lg': ['1.2125rem', { lineHeight: '1.75rem' }],    // ~19px (was 18px)
+        'xl': ['1.3375rem', { lineHeight: '1.875rem' }],   // ~21px (was 20px)
+        '2xl': ['1.6125rem', { lineHeight: '2.125rem' }],  // ~26px (was 24px)
+        '3xl': ['2.0125rem', { lineHeight: '2.5rem' }],    // ~32px (was 30px)
+        '4xl': ['2.4125rem', { lineHeight: '2.75rem' }],   // ~39px (was 36px)
+        '5xl': ['3.225rem', { lineHeight: '1' }],          // ~52px (was 48px)
+        '6xl': ['4.0125rem', { lineHeight: '1' }],         // ~64px (was 60px)
+        '7xl': ['4.8125rem', { lineHeight: '1' }],         // ~77px (was 72px)
+        '8xl': ['6.4375rem', { lineHeight: '1' }],         // ~103px (was 96px)
+        '9xl': ['8.0625rem', { lineHeight: '1' }],         // ~129px (was 120px)
+      },
       colors: {
         gray: {
           50: '#f9fafb',
